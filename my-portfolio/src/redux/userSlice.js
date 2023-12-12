879298ed082d20c656328e23785bd6487f5b9f6f
Harisundar S <harisundar12345678@gmail.com>
2023-12-12 17:59:48 +0530

    feat:redux setup completed

---
+++ my-portfolio/src/redux/userSlice.js
@@ -0,0 +1,20 @@
+// src/redux/userSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    name: '',
+    email: '',
+    // add more fields as needed
+  },
+  reducers: {
+    updateUser: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
+
+export const { updateUser } = userSlice.actions;
+export const selectUser = (state) => state.user;
+export default userSlice.reducer;
